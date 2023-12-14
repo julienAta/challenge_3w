4c5cdabc9dbe708b9a4eb6d518569fdb422ffd6c
jjwebsolutions <jj.websolutions@proton.me>
2023-12-14 10:03:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import Wrapper from "./components/Wrapper";
+import Number from "./components/Number";
+import CounterButton from "./components/CounterButton";
+
+import Students from "./components/Students";
+function App() {
+  return (
+    <>
+      <div>Challenge</div>
+    </>
+  );
+}
+
+export default App;
