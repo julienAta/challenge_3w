98a997042d0e3223c14e80c568d18ade0c9ff338
jjwebsolutions <jj.websolutions@proton.me>
2023-12-14 14:03:55 +0100

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
-import Wrapper from "./components/Wrapper";
-import Number from "./components/Number";
-import CounterButton from "./components/CounterButton";
-
-import Students from "./components/Students";
 function App() {
+  const [numbers, setNumbers] = useState([]);
+  const [pairs, setPairs] = useState([]);
+  const maxNumbers = 10;
+
+  const addNumber = () => {
+    if (numbers.length < maxNumbers) {
+      const newNumber = Math.floor(Math.random() * 30) + 1;
+      if (!numbers.includes(newNumber)) {
+        setNumbers([...numbers, newNumber]);
+      }
+    } else {
+      alert("La liste a atteint son maximum");
+    }
+  };
+
+  const generatePairs = () => {
+    let tempPairs = [];
+    for (let i = 0; i < numbers.length; i += 2) {
+      tempPairs.push(numbers.slice(i, i + 2));
+    }
+    setPairs(tempPairs);
+  };
+
+  const clearList = () => {
+    setNumbers([]);
+    setPairs([]);
+  };
+
   return (
     <>
-      <div>Challenge</div>
+      <div>
+        <button onClick={addNumber}>Ajouter un nombre</button>
+        <button onClick={clearList}>Supprimer la liste</button>
+        <div>Liste : {numbers.join(", ")}</div>
+        <button onClick={generatePairs}>Générer les binômes</button>
+        <div>
+          {pairs.map((pair, index) => (
+            <div key={index}>{pair.join(", ")}</div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
